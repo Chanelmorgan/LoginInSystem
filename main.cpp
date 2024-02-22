524d12449dcae56c3931fa028132aaba275e5af3
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-22 16:55:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+
+// Functions
+void login();
+void registration();
+void forgot();
+
+int main() {
+
+    int choose;
+
+
+    return 0;
+}
