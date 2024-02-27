c136f42878c888791b8ed29a8cc349dcd632d5ba
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-27 12:31:03 +0000

    starting the user interface

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ void forgot();
 int main() {
 
     int choose;
+    cout << "\t\t\t_______________________________________________________\n\n\n";
+    cout << "\t\t\t           Welcome to the Login in Page                 \n\n\n";
+    cout << "\t\t\t                    Menu:                               \n\n\n";
+    cout << "                                                               \n\n\n";
 
 
     return 0;
